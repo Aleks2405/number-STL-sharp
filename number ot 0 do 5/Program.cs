7bc2bf828e722550480cbf9120e42074f6ed0cb2
Aleksandr <shura.gorodnichev@mail.ru>
2022-02-01 01:13:56 +0300

    Добавьте файлы проекта.

---
+++ number ot 0 do 5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;  // без этого никак 
+
+using number_ot_0_do_5;
+/*Пользователь вводит словами цифру от 0 до 9. Приложение должно перевести слово в цифру. Например, если 
+пользователь ввёл five, приложение должно вывести на 
+экран 5.*/
+namespace number_ot_0_do_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string slovo;
+            Console.WriteLine("Введите слово");
+            slovo = Console.ReadLine();
+            var number = new Dictionary<string,int>()
+    {
+                 {"zero",0},
+                 { "one",1},
+                 { "two",2},
+                 {"tree",3},
+                 {"four",4},                 /* как stl map из с++*/
+                 {"five",5},
+                 {"six",6},
+                 {"seven",7},
+                 {"eight",8},
+                 {"nine",9}
+    };
+            int sam = number[slovo];
+            Console.WriteLine(sam);
+            //правда пытался сделать сам но увы решил погуглить и посмотреть библиотеки типо stl
+        }
+    }
+}
